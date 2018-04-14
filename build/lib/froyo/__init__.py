a44af65aee6f64426d28680d28c312f78243edc3
Kartikye Mittal <kartikye.mittal@gmail.com>
2018-04-13 18:09:56 -0700

    fixed many issues and added proper console commands and folder naem handling

--- build/lib/froyo/__init__.py
+++ build/lib/froyo/__init__.py
@@ -6,11 +6,12 @@ from PIL import Image
 from css_html_js_minify import html_minify, js_minify, css_minify
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import argparse
 
 source_global = ''
 dest_global = ''
 
-debug = False
+debug = True
 print_original = print
 def print_new(*stuff, sep=' ', end='\n', file=sys.stdout, flush=False, override=False):
     if debug or override:
@@ -78,11 +79,18 @@ def process(source, dest, filetype='all'):
         dirs[:] = [d for d in dirs if not d[0] == '.']
         for file in files:
             filepath = os.path.join(subdirs, file)
-            if file.endswith('html'):
+            if file.endswith('.html'):
                 process_html(filepath, source, dest)
-            elif not file.startswith('.'):
+            elif file.endswith('.css') and not file.endswith('.scss'):
+                process_css(filepath, source, dest)
+            elif file.endswith(('.jpg', '.png', '.gif', '.bmp', '.tiff', '.jpeg')):
+                process_image(filepath, source, dest)
+            elif file.startswith('.') or file.endswith(('.scss', '.psd')):
+                pass
+            else:
                 cp_dest = os.path.join(dest, filepath[len(source)+1:])
                 copy_file(filepath, cp_dest)
+                print(filepath)
     print("done")
 
 def process_html(filepath, source, dest):
@@ -121,7 +129,7 @@ def process_image(filepath, source, dest):
     image = Image.open(filepath)
     dest = os.path.join(dest, filepath[len(source)+1:])
     os.makedirs(os.path.dirname(dest), exist_ok=True)
-    image.save(dest, optimize=True, quality=90)
+    image.save(dest, optimize=True, quality=80)
 
 def copy_file(file, path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -132,19 +140,34 @@ def save_file(file, path):
     with open(path, 'w+', encoding='utf-8') as dest_file:
         dest_file.write(file)
 
-def main():
-    print("froyo is starting", override=True)
-    global source_global, dest_global
-    if len(sys.argv) < 3:
-        print("please run the following: froyo <SOURCE DIR> <DEST DIR>", override=True)
-        exit()
-    source = sys.argv[1]
-    if len(sys.argv) > 2:
-        dest = sys.argv[2]
+def clean_directory_name(directory):
+    if directory.endswith('\\') or directory.endswith('/'):
+        directory = directory[:-1]
+    if directory.startswith('./') or directory.startswith('.\\'):
+        directory = directory[2:]
+    return directory
+
+def main():    
+    parser = argparse.ArgumentParser(description='froyo - site distributor')
+
+    parser.add_argument('source', action="store")
+    parser.add_argument('dest', action="store", nargs='?')
+    parser.add_argument('--watch', action="store_true", dest="watch", default=False)
+
+    args = parser.parse_args()
+
+    print(args.dest)
+
+    source = clean_directory_name(args.source)
+    
+    if args.dest:
+        dest = clean_directory_name(args.dest)
     else:
-        dest = None
+        dest = source + '_dist'
 
     cwd = os.getcwd()
+
+    global source_global, dest_global
 
     source_global = os.path.join(cwd, source)
 
@@ -155,19 +178,19 @@ def main():
     dest_global = os.path.join(cwd, dest)
     
     if not os.path.isdir(dest_global):    
-        print('dest dir does not exist', override=True)
-        exit()
+        os.makedirs(dest_global)
 
     process(source_global, dest_global)
 
-    event_handler = DirectoryUpdate()
+    if args.watch:
+        event_handler = DirectoryUpdate()
 
-    observer = Observer()
-    observer.schedule(event_handler, source, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+        observer = Observer()
+        observer.schedule(event_handler, source, recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
