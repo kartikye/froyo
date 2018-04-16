d2a1ef5d1d7686591003d5b5cd4295fa2455b3d5
Kartikye Mittal <kartikye.mittal@gmail.com>
2018-04-15 20:56:14 -0700

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,12 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.1.0',  # Required
 
+    install_requires=[
+        'css-html-js-minify',
+        'Pillow',
+        'watchdog'
+    ],
+
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
